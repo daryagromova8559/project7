0f768f6c5853c6e87ac19392d07c3b51bfb23438
dasha gromova <darya.gromova8559@gmail.com>
2024-07-23 10:57:11 +0300

    init

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,19 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.apps import CatalogConfig
-from catalog.views import ProductListView
+from catalog.apps import NewappConfig
+from catalog.views import (ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView)
 from catalog.views import contacts
-from catalog.views import ProductDetailView
 
-app_name = CatalogConfig.name
+app_name = NewappConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='home'),
+    path('', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('contacts/', contacts, name='contacts'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
 
 from catalog.models import Product
 
 
 class ProductListView(ListView):
     model = Product
-    template_name = 'catalog/home.html'
+    template_name = 'catalog/product_list.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product_detail.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'price')
+    success_url = reverse_lazy('catalog:product_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'price')
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:product_list')
+
 
 def contacts(request):
     if request.method == 'POST':
